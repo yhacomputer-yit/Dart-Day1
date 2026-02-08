f7f7e957090efb2c4a5a9073ad330672c55f0c4e
yha computer <yhacomputer@gmail.com>
2026-02-08 12:51:22 +0630

    day 1 function

--- day1.dart
+++ day1.dart
@@ -1,9 +1,3 @@
-// void main(){
-//   print('Hello dart');
-// }
-void main() {
-void userProfile({required String name, int? age, String city = 'New York'}) {
-print('$name, ${age ?? 'Unknown'} years old, from $city');
-}
-userProfile(name: "Mg mg");
+void main(){
+  print('Hello dart');
 }
